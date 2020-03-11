f446af1160e2ba89dd2cd6e7ca88215dc60547cb
kevin cox <kevincox1983@gmail.com>
2020-03-10 20:05:45 -0500

    added recently searched cities and jumbotronto Index.html

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-
-// $("#searchButton").on("click", function(){
-
-
-// $.ajax({
-//     url: "http://api.openweathermap.org/data/2.5/weather?q=austin&appid=6e14dbb56496334f96d85f2b8fc4a0ed",
-//     method: "GET"
-//   }).then(function(response) {
-//     console.log(response);
-  
-// });
-
-// })
 //Search city and add to log in console city name
 $("#searchButton").on("click", function(){
     
@@ -20,14 +7,9 @@ let cityInput = $("#cityInput").val();
         method: "GET"
     }).then(function(response) {
         console.log(response);
-        let createButton = $("<button>").text("");
+        let createButton = $("<button type=\"button\" class=\"btn btn-info btn-lg btn-block\">Block level button</button>").text("");
         createButton.append(response.name);
         $("article").append(createButton);
+       
     });
 })
-//create table of searched cities
-
-let createButton = function(response){
-    let cityButton =$("<button>");
-    $("section").append(response);
-}
