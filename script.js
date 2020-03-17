cb10e65d2fd9a763548d0e8a59fe6ccda199bf2f
kevin cox <kevincox1983@gmail.com>
2020-03-16 22:22:21 -0500

    updated script

--- script.js
+++ script.js
@@ -10,6 +10,22 @@ let cityInput = $("#cityInput").val();
         let createButton = $("<button type=\"button\" class=\"btn btn-info btn-lg btn-block\">Block level button</button>").text("");
         createButton.append(response.name);
         $("article").append(createButton);
+        
        
     });
-})
+
+
+//Return city weather info seperate container
+
+        $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=6e14dbb56496334f96d85f2b8fc4a0ed",
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            let createButton = $("<button type=\"button\" class=\"btn btn-info btn-lg btn-block\">Block level button</button>").text("");
+            createButton.append(response.name);
+            $("#currentWeather").append(createButton);
+            
+           
+        });
+    });
